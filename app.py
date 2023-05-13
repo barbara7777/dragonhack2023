99e16ed5c86deba15105ebb85be6cdc224476a33
benjamin-lipnik <=>
2023-05-13 22:50:22 +0200

    More gpt, more better

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from database import *
 from room import *
 import qrcode
 import os
+from gpt import *
 
 app = Flask(__name__)
 rooms_dict = { }
@@ -12,20 +13,15 @@ rooms_dict = { }
 def room_to_json(room):
 	return "{{ id : {}, users: {} }}\n".format(room.id, str(room.users))
 
-
 def events_to_json(events):
 	return str(list(events.values()))
-
 
 def key_to_intkey(key):
 	return sum(map(ord, list(key)))
 
-
-
 @app.route('/event', methods=['POST', 'GET'])
 def show_events():
 	return str(list(events.values()))
-
 
 @app.route('/cleanup')
 def cleanup():
@@ -43,7 +39,6 @@ def qr_codes(id):
 @app.route('/', methods=['POST', 'GET'])
 def home():
 	return "<button>Ustvari sobo</button>"
-
 
 @app.route('/room', methods=['POST', 'GET'])
 def show_rooms():
@@ -73,8 +68,7 @@ def prijava(room_id):
 
 	return room_to_json(room)
 
-
-@app.route('/rank-event/<room_id>', methods=['POST', 'GET'])
+@app.route('/rank-events/<room_id>', methods=['POST', 'GET'])
 def rank_events(room_id):
 	room_id = key_to_intkey(room_id)
 	if(room_id not in rooms_dict):
@@ -85,3 +79,53 @@ def rank_events(room_id):
 	calculate_rank(attendants)
 
 	return events_to_json(events)
+
+@app.route("/organise/<room_id>/<event_id>", methods=['POST', 'GET'])
+def organize(room_id, event_id):
+	try:
+		room_id = key_to_intkey(room_id)
+		event_id = int(event_id)
+	except:
+		return "Invalid room or event id."
+
+	if(room_id not in rooms_dict):
+		return "Invalid room"
+	if(event_id >= len(events)):
+		return "Invalid event"
+
+	room = rooms_dict[room_id]
+	event = events[event_id]
+
+	##mkay tu zdej ustvarimo seznam in razporeditve
+	status, result = gpt_create_arrangement(len(room.users), event.title)
+	if(status == 1):
+		return result
+	return "Opa, neki ni ok.\n"
+
+@app.route("/invite/<room_id>/<event_id>", methods=['POST', 'GET'])
+def invite(room_id, event_id):
+	try:
+		room_id = key_to_intkey(room_id)
+		event_id = int(event_id)
+	except:
+		return "Invalid room or event id."
+
+	if(room_id not in rooms_dict):
+		return "Invalid room"
+	if(event_id >= len(events)):
+		return "Invalid event"
+
+	if "user" not in request.form:
+		return "Invalid user"
+	user_id = int(request.form["user"])
+	user = people[user_id]
+
+	room = rooms_dict[room_id]
+	event = events[event_id]
+
+	status, result = gpt_create_invitation(user.name, event.title, event.date)
+	if(status == 1):
+		return result
+	return "Opa, neki ni ok.\n"
+
+
